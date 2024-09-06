187e0da7735d0333c222836bac9031f30c82ff3f
JON <jalvarez-2023415@kinal.edu.gt>
2024-09-05 21:04:28 -0600

    Pagination

--- src/components/MarvelApp.jsx
+++ src/components/MarvelApp.jsx
@@ -12,16 +12,16 @@ export const MarvelApp = () => {
   }
   return (
     <>
-      
+      <nav class="navbar bg-body-tertiary" >
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjmHn7hDycWvYvGnj50dxygz2EUz8MBKKCqg&s" alt="Logo" width="150" height="70" class="d-inline-block align-text-top alig-items-left mt-10 w-10" />
+          <div className='container d-flex flex-row justify-content-center alig-items-center mt-3 w-50'>
+            <form className='d-flex' onSubmit={(e) => { handlePoke(categoria, e) }} >
+              <input type='text' className='form-control me-2'  placeholder="Nombre del superheroe" onChange={(e) => { setCategoria(e.target.value) }} />
+              <input type='submit' className='btn btn-outline-danger' value='Buscar' />
 
-      <div className='container d-flex flex-row justify-content-center alig-items-center mt-3 w-50'>
-
-        <form className='d-flex' onSubmit={(e) => { handlePoke(categoria, e) }} >
-          <input type='text' className='form-control me-2' onChange={(e) => { setCategoria(e.target.value) }} />
-          <input type='submit' className='btn btn-outline-success' value='Buscar' />
-        </form>
-
-      </div>
+            </form>
+          </div>
+      </nav>
       <GridCharacters characters={characters} />
       <Pagination className='position-absolute start-50' count={pag} page={page} onChange={handlePageChange} />
     </>
